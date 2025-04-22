63d3baa26a8151a24213b0216b56043cf20b5ecb
Gerard-Laptop <gerardpozo06@gmail.com>
2025-04-22 23:51:36 +0200

    Añadir hibernate y configuraciones, comentar clase Jugador

--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -6,17 +6,67 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+/**
+ * Classe que permet crear un jugador.
+ * <p>
+ * Un jugador té que tenir un nom i un nombre de punts.
+ * </p>
+ * <p>
+ * Incorpora un constructor per indicar el nom i nombre de punts que té el
+ * jugador <b>{@link #Jugador(String, int)}</b>.
+ * </p>
+ * <p>
+ * Incorpora etiquetes d'Hibernate per poder utilitzar-lo directament.
+ * <br>
+ * El id del jugador és autoincremental, la classe la incorpora però no es pot
+ * modificar o canviar, quan es declara un jugador comença en 1 i es va
+ * incrementant.
+ * </p>
+ */
 @Entity(name = "jugadors")
 public class Jugador {
 
+    /**
+     * El id del jugador, es autoincrementa
+     */
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_jugador;
+    /**
+     * Nom del jugador
+     */
     @Column(name = "nom")
     private String nom;
+    /**
+     * Punts del jugador
+     */
     @Column(name = "punts")
     private int punts;
 
+    /**
+     * Retorna el nom del jugador
+     * 
+     * @return Nom del jugador
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Retorna els punts que té el jugador
+     * 
+     * @return Punts que ha aconseguit el jugador
+     */
+    public int getPunts() {
+        return punts;
+    }
+
+    /**
+     * Constructor Jugador, estableix el nom i els punts que té el jugador.
+     * 
+     * @param nom Nom del jugador
+     * @param punts Punts del jugador
+     */
     public Jugador(String nom, int punts) {
         this.nom = nom;
         this.punts = punts;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.example.connectorHibernate.AccesDades;
+import org.example.connectorHibernate.ConnectorHibernate;
 
 public class Main {
     public static void main(String[] args) {

6953b2d1adfaa80e90f315068af73898a0311be1
ycazalla1 <ycazaa1@gmail.com>
2025-04-24 18:13:39 +0200

    Moure una barra amb W/S

---
+++ src/main/java/org/example/Racquet.java
@@ -0,0 +1,40 @@
+package org.example;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Racquet {
+    private final static int MIDA_AMPLADA = 30, MIDA_ALTURA = 150;
+    private int x, y, ya = 0;
+    private PanellJoc panellJoc;
+
+    public void setYa(int ya) {
+        this.ya = ya;
+    }
+
+    public Racquet(int x, int y, PanellJoc panellJoc){
+        this.x = x;
+        this.y = y;
+        this.panellJoc = panellJoc;
+    }
+
+    public void raqcquetMoviment(){
+        if (y + ya > 0 && y + ya < panellJoc.getHeight()-MIDA_ALTURA){
+            y += ya;
+        }
+    }
+
+    public void paint(Graphics g){
+        g.fillRect(x, y, MIDA_AMPLADA, MIDA_ALTURA);
+    }
+
+    public void keyReleased(KeyEvent e) {
+        ya = 0;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W)
+            ya = -1;
+        if (e.getKeyCode() == KeyEvent.VK_S)
+            ya = 1;
+    }
+}
---
+++ src/main/java/org/example/connectorHibernate/acces.java
@@ -0,0 +1,27 @@
+package org.example.connectorHibernate;
+import org.example.Jugador;
+import java.sql.*;
+
+public class acces {
+    public static void afegirInformacio(Jugador j){
+        try {
+            String nom = "usuari";
+            String contrasenya = "1234";
+            String url = "jdbc:mysql://localhost:3306/retrotenis";
+            Connection conexio = DriverManager.getConnection(url, nom, contrasenya);
+
+            String consulta = "insert into taula (nom_usuari, puntuacio) values (?, ?)";
+            PreparedStatement pr = conexio.prepareStatement(consulta);
+            pr.setString(1, j.getNom());
+            pr.setInt(2, j.getPunts());
+
+            Statement st = conexio.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            st.execute(consulta);
+            pr.executeUpdate();
+            conexio.close();
+        }
+        catch (SQLException e){
+            System.out.println(e.toString());
+        }
+    }
+}

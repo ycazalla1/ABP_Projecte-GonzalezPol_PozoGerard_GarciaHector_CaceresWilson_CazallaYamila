12b26b40f0bbbf393ef2c0a3d1e2f272a1838a0f
ycazalla1 <ycazaa1@gmail.com>
2025-04-29 16:19:57 +0200

    Joc "realitzat" fent millores + finestra d'inici del joc

---
+++ src/main/java/org/example/Finals.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Finals {
+
+    public static final int MARGES = 30;
+    public static final int TAMANY_ALT_RACQUET = 150;
+    public static final int TAMANY_AMPLE_RACQUET = 30;
+    /**
+     * Indica l'espai que hi ha entre una racquet i el marge del rectangle
+     */
+    public static final int MARGE_RACQUET_RECTANGLE = 10;
+
+}
---
+++ src/main/java/org/example/menu/MenuProva.java
@@ -0,0 +1,20 @@
+package org.example.menu;
+
+import org.example.PanellJoc;
+
+import javax.swing.*;
+
+public class MenuProva {
+
+    JFrame jf = new JFrame();
+
+    public MenuProva() {
+        jf.setTitle("Menú inici");
+        jf.setSize(1280, 800);
+        jf.setResizable(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+
+}
